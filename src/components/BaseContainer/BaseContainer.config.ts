2461d312150339c80495ef3d217008d75ce54e2b
Liam Hall <liam@whirli.com>
2022-02-01 13:15:14 +0000

    [WHIR-3121]Tidy up imports

--- src/components/BaseContainer/BaseContainer.config.ts
+++ src/components/BaseContainer/BaseContainer.config.ts
@@ -1,32 +1,13 @@
 // @ts-ignore
-import { PropType } from '@composition';
-import * as PropHelpers from '@whirli-components/helpers/props';
 import { ComponentStyles, ComponentProps } from '@whirli-components/@types/components';
 
 // Local config
 import config from '@whirli-local/components/BaseContainer/BaseContainer.config';
 
-// Package config
-import { PropKeys, PropValues } from './BaseContainer.constants';
-// import { PropColor } from './BaseContainer.types';
-
 export const ConfigStyles: ComponentStyles = {
   ...config.styles,
-  /*[PropKeys.COLOR]: {
-    generateBreakpoints: true,
-    classes: {
-      default: 'color-black',
-      ...config.styles[PropKeys.COLOR]?.classes,
-    } as Record<PropColor, string>,
-  },*/
 };
 
 export const ConfigProps: ComponentProps = {
-  /*[PropKeys.COLOR]: {
-    required: false,
-    type: [String, Object] as PropType<PropColor>,
-    default: 'default',
-    ...PropHelpers.breakpointStylePropValidation(ConfigStyles, PropKeys.COLOR),
-  },*/
   ...config.props,
 };
